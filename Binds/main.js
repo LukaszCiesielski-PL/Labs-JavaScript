72725f57d84f0d5f09a49e847943b6220aa2233e
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2020-10-29 02:34:55 +0100

    js

--- Binds/main.js
+++ Binds/main.js
@@ -1,58 +1,76 @@
 document.body.addEventListener("keypress", onKeyPress);
-document.querySelector('#recordBtn').addEventListener("click", onRecordBtn);
+document.querySelector("#recordBtn").addEventListener("click", onRecordBtn);
 document.querySelector("#playBtn").addEventListener("click", onPlayBtn);
 
 let recordedSound = [];
 let recordStartTime;
 function onKeyPress(ev) {
-    let soundId;
-    switch (ev.code) {
-        case "KeyA":
-            soundId = "boom";
-            /*document.querySelector(`.${soundId}`).classList.toggle('sound');*/
-            break;
-        case "KeyS":
-            soundId = "clap";
+  let soundId;
+  switch (ev.code) {
+    case "KeyA":
+      soundId = "boom";
+      /*document.querySelector(`.${soundId}`).classList.toggle('sound');*/
+      break;
+    case "KeyS":
+      soundId = "clap";
 
-            break;
-        case "KeyD":
-            soundId = "hihat";
+      break;
+    case "KeyD":
+      soundId = "hihat";
 
-            break;
-        case "KeyF":
-            soundId = "kick";
+      break;
+    case "KeyF":
+      soundId = "kick";
 
-            break;
-    }
-    if (soundId) {
-        const soundTime = Date.now() - recordStartTime;
-        const soundObj = {
-            soundId: soundId,
-            time: soundTime,
-            
-        };
-        
-        playSound(soundId);
-        
-        recordedSound.push(soundObj);
-    }
+      break;
+    case "KeyG":
+      soundId = "openhat";
+
+      break;
+    case "KeyH":
+      soundId = "ride";
+
+      break;
+    case "KeyJ":
+      soundId = "snare";
+
+      break;
+    case "KeyK":
+      soundId = "tink";
+
+      break;
+    case "KeyL":
+      soundId = "tom";
+
+      break;
+    
+  }
+  if (soundId) {
+    const soundTime = Date.now() - recordStartTime;
+    const soundObj = {
+      soundId: soundId,
+      time: soundTime,
+    };
+
+    playSound(soundId);
+
+    recordedSound.push(soundObj);
+  }
 }
 
 function onRecordBtn() {
-    recordStartTime = Date.now();
+  recordStartTime = Date.now();
 }
 function onPlayBtn() {
-    for (let index = 0; index < recordedSound.length; index++) {
-        const soundObj = recordedSound[index];
-        setTimeout(() => {
-            playSound(soundObj.soundId);
-        }, soundObj.time);
-    }
+  for (let index = 0; index < recordedSound.length; index++) {
+    const soundObj = recordedSound[index];
+    setTimeout(() => {
+      playSound(soundObj.soundId);
+    }, soundObj.time);
+  }
 }
 
 function playSound(soundId) {
-    const sound = document.querySelector("#" + soundId);
-    sound.play();
+  const sound = document.querySelector("#" + soundId);
+  sound.play();
 }
-
-
