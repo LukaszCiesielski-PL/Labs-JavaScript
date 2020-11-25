0b6746710f4725be87a748afbe8ce9499106e9b9
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2020-11-25 23:43:12 +0100

    Change SVG to Canvas
    Convert to canvas:
    Delete SVG in HTML / JS

--- Game Ball and Hole/main.js
+++ Game Ball and Hole/main.js
@@ -1,31 +1,8 @@
-let menu = document.querySelector("#menu");
-let ball= document.querySelector("#ball");                     
-let speedX = 0, speedY = 0;
-let posX = 50, posY = 90;
-window.addEventListener('deviceorientation', changePosition)
 
-function start(){ 
-                                                                       
-    move();                      
-    document.getElementById("start").hidden=true;  
-}
+    
 
-function changePosition(e){            
-    console.log(e);
-    speedX=e.gamma/45
-    speedY=e.beta/45
-}
-
-function move(){                 
-    if(posX+speedX<window.innerWidth-50 && posX+speedX>0){  
-        posX+=speedX;
-        ball.style.cx=posX+'%';        
-    }
-    if(posY+speedY<window.innerHeight-50 && posY+speedY>0){
-        posY+=speedY;
-        ball.style.cy=posY+'%';        
-    }
-    window.requestAnimationFrame(move)
-}
-
+    
+    
+    
+    
 
