60da9e6057c843c14964015fc8d9bd23ace1f1b7
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2020-12-17 00:35:13 +0100

    Update main.js

--- AnimateSnow/main.js
+++ AnimateSnow/main.js
@@ -85,7 +85,7 @@ class Snow{
     }
     
     random = function (max, min) {
-        return Math.random() * (max - min) + min;
+        return Math.random() * (max - min +1) + min;
     };
         
     
