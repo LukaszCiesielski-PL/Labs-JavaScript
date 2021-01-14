cc8fddfa4fd6ea0bf7d4fbdf753bc653e90667b6
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2021-01-14 11:36:24 +0100

    Update main.js
    Dodanie kanałów odtwarzania dźwięku

--- Drumkit/main.js
+++ Drumkit/main.js
@@ -2,8 +2,45 @@ document.body.addEventListener('keypress', onKeyPress);
 document.querySelector('#recordBtn').addEventListener('click', onRecordBtn);
 document.querySelector('#playBtn').addEventListener('click', onPlayBtn);
 
-let recordedSound = [];
-let recordStartTime;
+//let recordedSound = [];
+
+const recordedStartsTime = [
+    {
+        id: 'channelOne',
+        time: NaN,
+    },
+    {
+        id: 'channelTwo',
+        time: NaN,
+    },
+    {
+        id: 'channelThree',
+        time: NaN,
+    },
+    {
+        id: 'channelFour',
+        time: NaN,
+    },
+];
+
+const recordedSounds = [
+    {
+        id: 'channelOne',
+        sounds: [],
+    },
+    {
+        id: 'channelTwo',
+        sounds: [],
+    },
+    {
+        id: 'channelThree',
+        sounds: [],
+    },
+    {
+        id: 'channelFour',
+        sounds: [],
+    },
+];
 
 function onKeyPress(ev) {
     const sound = document.querySelector('.boom');
@@ -21,54 +58,72 @@ function onKeyPress(ev) {
     case 'KeyA':
         soundId = 'boom';
         sound.classList.add('soundhover');
-        setTimeout(function(){ sound.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound.classList.remove('soundhover');
+        }, 400);
         break;
-        
+
     case 'KeyS':
         soundId = 'clap';
         sound2.classList.add('soundhover');
-        setTimeout(function(){ sound2.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound2.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyD':
         soundId = 'hihat';
         sound3.classList.add('soundhover');
-        setTimeout(function(){ sound3.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound3.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyF':
         soundId = 'kick';
         sound4.classList.add('soundhover');
-        setTimeout(function(){ sound4.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound4.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyG':
         soundId = 'openhat';
         sound5.classList.add('soundhover');
-        setTimeout(function(){ sound5.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound5.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyH':
         soundId = 'ride';
         sound6.classList.add('soundhover');
-        setTimeout(function(){ sound6.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound6.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyJ':
         soundId = 'snare';
         sound7.classList.add('soundhover');
-        setTimeout(function(){ sound7.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound7.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyK':
         soundId = 'tink';
         sound8.classList.add('soundhover');
-        setTimeout(function(){ sound8.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound8.classList.remove('soundhover');
+        }, 400);
         break;
     case 'KeyL':
         soundId = 'tom';
         sound9.classList.add('soundhover');
-        setTimeout(function(){ sound9.classList.remove('soundhover'); }, 400);
+        setTimeout(function () {
+            sound9.classList.remove('soundhover');
+        }, 400);
         break;
-
     }
-    
+
     if (soundId) {
-        const soundTime = Date.now() - recordStartTime;
+        const timeStart = recordedStartsTime.find((el) => el.id == currentChannel).time;
+        const soundTime = Date.now() - timeStart;
         const soundObj = {
             soundId: soundId,
             time: soundTime,
@@ -76,20 +131,23 @@ function onKeyPress(ev) {
 
         playSound(soundId);
 
-        recordedSound.push(soundObj);
+        recordedSounds.find((el) => el.id == currentChannel).sounds.push(soundObj);
     }
-    
 }
 
 function onRecordBtn() {
-    recordStartTime = Date.now();
+    recordedStartsTime.find((el) => el.id == currentChannel).time = Date.now();
 }
+
 function onPlayBtn() {
-    for (let index = 0; index < recordedSound.length; index++) {
-        const soundObj = recordedSound[index];
-        setTimeout(() => {
-            playSound(soundObj.soundId);
-        }, soundObj.time);
+    for (let sound of recordedSounds) {
+        let soundArr = sound.sounds;
+        for (let index = 0; index < soundArr.length; index++) {
+            const soundObj = soundArr[index];
+            setTimeout(() => {
+                playSound(soundObj.soundId);
+            }, soundObj.time);
+        }
     }
 }
 
@@ -98,3 +156,12 @@ function playSound(soundId) {
     sound.play();
 }
 
+const channels = document.querySelectorAll('.ch input');
+let currentChannel;
+
+channels.forEach((channel) => {
+    channel.addEventListener('change', (element) => {
+        currentChannel = element.target.id;
+        console.log(currentChannel);
+    });
+});
