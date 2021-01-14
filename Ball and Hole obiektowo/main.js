78f47b13b5ad23a1d3ca6ff4d2f3f4bbd383fbbe
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2021-01-14 14:33:25 +0100

    update JS
    dodatkowe kolizje z pechowymi dziurami

--- Ball and Hole obiektowo/main.js
+++ Ball and Hole obiektowo/main.js
@@ -9,7 +9,10 @@ canvas.height = window_height;
 
 canvas.style.background = "#000000";
 
-
+let Distance = function(posX, posY, posX2, posY2){
+    const result = Math.sqrt(Math.pow(posX2 - posX, 2) + Math.pow(posY2 - posY, 2));
+    return result;
+}
 
 class Circle{
     constructor(posX, posY, radius, color, speed){
@@ -54,6 +57,11 @@ class Circle{
         this.posY += this.dy;
     }
     
+    timeCount(){
+        
+        
+    }
+
     move(){
         window.addEventListener('deviceorientation', e=>{
             console.log(e);
@@ -62,18 +70,28 @@ class Circle{
         })
     }
     
+    
 }
 
-let Distance = function(posX, posY, posX2, posY2){
-    const result = Math.sqrt(Math.pow(posX2 - posX, 2) + Math.pow(posY2 - posY, 2));
-    return result;
-}
 
-let circle = new Circle(0.5*window_width, 0.5*window_height, 30, "red", 0);
-let circle2 = new Circle(0.1*window_width, 0.5*window_height, 60, "red", 0);
+
+let circle = new Circle(0.5*window_width, 0.5*window_height, 30, "white", 0);
+let circle2 = new Circle(0.1*window_width, 0.5*window_height, 60, "white", 0);
+let circle3 = new Circle(0.3*window_width, 0.5*window_height, 30, "red", 0);
+let circle4 = new Circle(0.3*window_width, 0.2*window_height, 30, "red", 0);
+let circle5 = new Circle(0.3*window_width, 0.8*window_height, 30, "red", 0);
+let circle6 = new Circle(0.2*window_width, 0.5*window_height, 30, "red", 0);
+let circle7 = new Circle(0.2*window_width, 0.2*window_height, 30, "red", 0);
+let circle8 = new Circle(0.2*window_width, 0.8*window_height, 30, "red", 0);
 
 circle.draw(context);
 circle2.draw(context);
+circle3.draw(context);
+circle4.draw(context);
+circle5.draw(context);
+circle6.draw(context);
+circle7.draw(context);
+circle8.draw(context);
 
 let updateCircle = function(){
     requestAnimationFrame(updateCircle);
@@ -81,12 +99,69 @@ let updateCircle = function(){
     
     circle.update();
     circle2.update();
+    circle3.update();
+    circle4.update();
+    circle5.update();
+    circle6.update();
+    circle7.update();
+    circle8.update();
 
-    if(Distance(circle.posX,circle.posY,circle2.posX,circle2.posY) < (circle2.radius + circle.radius)){
+    if(Distance(circle.posX,circle.posY,circle2.posX,circle2.posY) < (circle2.radius)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="blue";
+        circle2.color="blue"; 
+
+        alert("You Win !!!");
+    }
+    
+    if(Distance(circle.posX,circle.posY,circle3.posX,circle3.posY) < (circle3.radius+29)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="red";   
+        alert("You Lose !!!");  
+    }
+    if(Distance(circle.posX,circle.posY,circle4.posX,circle4.posY) < (circle4.radius+29)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="red";  
+        alert("You Lose !!!");   
+    }
+    if(Distance(circle.posX,circle.posY,circle5.posX,circle5.posY) < (circle5.radius+29)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="red"; 
+        alert("You Lose !!!");    
+    }
+    if(Distance(circle.posX,circle.posY,circle6.posX,circle6.posY) < (circle6.radius+29)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="red"; 
+        alert("You Lose !!!");    
+    }
+    if(Distance(circle.posX,circle.posY,circle7.posX,circle7.posY) < (circle7.radius+29)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="red";  
+        alert("You Lose !!!");   
+    }
+    if(Distance(circle.posX,circle.posY,circle8.posX,circle8.posY) < (circle8.radius+29)){
+        circle.speed = 0;
+        circle.dx = 0;
+        circle.dy = 0;
+        circle.color="red";   
         
+        alert("You Lose !!!");
     }
 }
 
+circle.timeCount();
 circle.move();
 updateCircle();
 
